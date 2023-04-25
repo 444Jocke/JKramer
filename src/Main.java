446708c2563562842558b955f88c87fc31d7c46e
444Jocke <joakim.karlsson.ken@gmail.com>
2023-04-25 14:52:26 +0200

    hello

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import javax.swing.JFrame;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        JFrame frame = new JFrame();
+        frame.setTitle("Agar.io");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setSize(600, 600);
+        frame.setVisible(true);
+
+        GamePanel gamePanel = new GamePanel();
+        frame.add(gamePanel);
+
+        frame.pack();
+
+        frame.getContentPane().setBackground(Color.YELLOW);
+
+        gamePanel.startGameThread();
+
     }
 }
